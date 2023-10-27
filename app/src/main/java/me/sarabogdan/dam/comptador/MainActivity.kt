d84761d5bf8e90e12d75a2fc1d901c4815795442
«sara21h» <sbogdan@iesebre.com>
2023-10-27 12:01:57 +0200

    Aplicació iniciada

---
+++ app/src/main/java/me/sarabogdan/dam/comptador/MainActivity.kt
@@ -0,0 +1,12 @@
+package me.sarabogdan.dam.comptador
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
