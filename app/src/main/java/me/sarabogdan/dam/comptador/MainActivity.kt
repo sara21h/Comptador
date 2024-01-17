ebca6b424409afd91f679ad681578d31f8592b50
«sara21h» <sbogdan@iesebre.com>
2024-01-17 16:16:42 +0100

    canvis colors, animació i icono

--- app/src/main/java/me/sarabogdan/dam/comptador/MainActivity.kt
+++ app/src/main/java/me/sarabogdan/dam/comptador/MainActivity.kt
@@ -6,10 +6,12 @@ import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.ComponentActivity
+import android.view.animation.AnimationUtils
+
 
 class MainActivity : ComponentActivity() {
 
-    private val INITIAL_TIME = 3
+    private val INITIAL_TIME = 10
 
     internal lateinit var tapMeButton : Button
     internal lateinit var counterTextView : TextView
@@ -32,6 +34,12 @@ class MainActivity : ComponentActivity() {
         timeTextView = findViewById(R.id.timeTextView)
         counterTextView = findViewById(R.id.counterTextView)
 
+        val fadeAnimation = AnimationUtils.loadAnimation(this, R.anim.fade)
+        tapMeButton.startAnimation(fadeAnimation)
+        timeTextView.startAnimation(fadeAnimation)
+        counterTextView.startAnimation(fadeAnimation)
+
+        counterTextView.text = getString(R.string.counterText, counter)
         tapMeButton.setOnClickListener{
             if (!appStarted){
                 startGame()
@@ -51,7 +59,7 @@ class MainActivity : ComponentActivity() {
         countdownTimer = object : CountDownTimer(initialCountDownTimer, intervalCountDownTimer){
             override fun onTick(millisUntilFinished: Long) {
                 val timeLeft = millisUntilFinished / 1000
-                timeTextView.text = timeLeft.toString()
+                timeTextView.text = getString(R.string.timeText, timeLeft)
             }
 
             override fun onFinish() {
@@ -61,10 +69,13 @@ class MainActivity : ComponentActivity() {
     }
     private fun incrementCounter(){
         counter += 1
-        counterTextView.text = counter.toString()
+        counterTextView.text = getString(R.string.counterText, counter)
     }
 
     private fun endGame(){
+        val fadeAnimation = AnimationUtils.loadAnimation(this, R.anim.blink)
+        timeTextView.startAnimation(fadeAnimation)
+
         Toast.makeText(this, getString(R.string.endGame), Toast.LENGTH_LONG).show()
         resetGame()
     }
